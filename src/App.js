86d1730beed7272c18fe1efa5184a28920b2f74d
Omer Gencoglu <omersaitgencoglu@gmail.com>
2023-01-10 17:00:51 -0500

    Add input field and add an item to the list functionality

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ function App() {
     });
   };
 
+  console.log(list);
+
   return (
     <Layout>
       <NewItem onAddItem={addItemHandler} />
--- src/components/NewItem/NewItem.js
+++ src/components/NewItem/NewItem.js
@@ -1,7 +1,33 @@
+import { useRef } from "react";
 import styles from "./NewItem.module.scss";
 
-function NewItem() {
-  return <div>NewItem</div>;
+function NewItem(props) {
+  const formRef = useRef();
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    const enteredValue = formRef.current.newItem.value;
+
+    // check if empty or includes only spaces
+    if (enteredValue && enteredValue.replace(/ /g, "")) {
+      props.onAddItem(enteredValue);
+    }
+    formRef.current.reset();
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler} ref={formRef}>
+      <label htmlFor="newItem" hidden>
+        New Item
+      </label>
+      <input
+        id="newItem"
+        type="text"
+        placeholder="Enter a new item.."
+        autoComplete="off"
+      />
+    </form>
+  );
 }
 
 export default NewItem;
