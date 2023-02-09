8f89a96dac1030c663f89b910f2b816b3701979c
Omer Gencoglu <omersaitgencoglu@gmail.com>
2023-02-09 15:32:37 -0500

    Add fallback text to the list

--- src/components/Items/ItemList.js
+++ src/components/Items/ItemList.js
@@ -5,26 +5,29 @@ import ItemCard from "./ItemCard";
 import styles from "./ItemList.module.scss";
 
 function ItemList(props) {
+  const filteredItems = props.items.filter((item) => {
+    if (props.filter === "todo") {
+      return !item.completed;
+    }
+    if (props.filter === "completed") {
+      return item.completed;
+    }
+    return item;
+  });
+
   return (
     <FlipMove typeName="ul" className={styles.list}>
-      {props.items
-        .filter((item) => {
-          if (props.filter === "todo") {
-            return !item.completed;
-          }
-          if (props.filter === "completed") {
-            return item.completed;
-          }
-          return item;
-        })
-        .map((item) => (
-          <ItemCard
-            key={item.id}
-            id={item.id}
-            text={item.text}
-            status={item.completed}
-          />
-        ))}
+      {filteredItems.map((item) => (
+        <ItemCard
+          key={item.id}
+          id={item.id}
+          text={item.text}
+          status={item.completed}
+        />
+      ))}
+      {(!filteredItems || !filteredItems.length) && (
+        <p className={styles.fallback}>There's nothing to show here.</p>
+      )}
     </FlipMove>
   );
 }
